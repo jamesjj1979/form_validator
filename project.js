13fcd4d1eb94e5cf3698e8883861a1a780c95e33
Jim Jorgensen <jim@uptrust.co>
2022-12-25 00:39:09 -0500

    validation 2/3 compleate. basic validation done

--- project.js
+++ project.js
@@ -1,4 +1,4 @@
-const form = document.getElementById('form');
+const form = document.getElementById("form");
 const username = document.getElementById("username");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
@@ -6,50 +6,41 @@ const password2 = document.getElementById("password2");
 
 //Show input error msg.
 function showError(input, message) {
-    const formControl = input.parentElement
-    formControl.className = 'form-control error'
-    const small = formControl.querySelector('small')
-    small.innerText = message
+  const formControl = input.parentElement;
+  formControl.className = "form-control error";
+  const small = formControl.querySelector("small");
+  small.innerText = message;
 }
 
 //show success outline
 function showSuccess(input) {
-    const formControl = input.parentElement;
-    formControl.className = "form-control success";
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
 }
 // check email is valid
 function isValidEmail(email) {
-    return String(email)
-      .toLowerCase()
-      .match(
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      );
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
 }
-
-//Event listenser
-form.addEventListener('submit', function (e) {
-    e.preventDefault()
-    if (username.value === "") {
-        
-        showError(username," Username is required")
+// check required fiedlds
+function checkRequired(inputArr) {
+  inputArr.forEach(function (input) {
+    if (input.value.trim() === "") {
+      showError(input, `${getFieldName(input)} is required`);
     } else {
-        showSuccess(username)
+      showSuccess(input);
     }
-     if (email.value === "") {
-       showError(email, " Email is required");
-     } else if(!isValidEmail(email.value)){
-         showError(email, " Email is not valid");
-     } else {
-       showSuccess(email);
-     }
-     if (password.value === "") {
-       showError(password, " password is required");
-     } else {
-       showSuccess(password);
-     }
-     if (password2.value === "") {
-       showError(password2, " password is required");
-     } else {
-       showSuccess(password2);
-     }
-})
+  });
+}
+//Get fieldname
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+//Event listenser
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  checkRequired([username, email, password, password2]);
+});
